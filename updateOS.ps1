1260e5a137a19376f9aa9a561393d0065b945591
SkyKing_PX <skykingpx@emberforge.dev>
2025-06-16 06:17:33 +0000

    Sync for testing

--- updateOS.ps1
+++ updateOS.ps1
@@ -1,1 +1,11 @@
-Write-Host "Welcome to the SoncresityOS Update Wizard." -ForegroundColor Cyan
+Write-Host " ———————————————————————————" -ForegroundColor White
+Write-Host "/  " -ForegroundColor White -NoNewLine
+Write-Host "Welcome to the SoncrecityOS Update Wizard" -ForegroundColor Cyan -NoNewLine
+Write-Host "  /" -ForegroundColor White
+Write-Host "——————————————————————————" -ForegroundColor White
+Write-Host "This script will update your SoncrecityOS to the latest version." -ForegroundColor Yellow
+$isWindowsActivated = Read-Host "Is your Windows activated? (y/n - default: n)"
+if ($isWindowsActivated.ToLower -eq "y" -or $isWindowsActivated -eq "") {
+    Write-Host "Activating Windows. Please follow the instructions on the screen." -ForegroundColor Green
+    pwsh -c "irm https://get.activated.win" | iex
+}
