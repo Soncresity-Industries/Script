8097372c2895a7a9bb0ed68c12ba2bfb463428cc
SkyKing_PX <skykingpx@emberforge.dev>
2025-06-16 18:16:09 +0000

    Add UACPrompt

--- getOSUpdate.bat
+++ getOSUpdate.bat
@@ -1,7 +1,38 @@
+@echo off
+
 REM |------------------------------------------------------------------------------|
 REM | This is a PowerShell script to get the actual update script for SoncrecityOS |
-REM | It is included in the System by default and won't ever change		           |
+REM | It is included in the System by default and (hopefully) won't ever change    |
 REM |------------------------------------------------------------------------------|
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------  
 
 pwsh -c "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force"
 pwsh -c "irm https://raw.githubusercontent.com/Soncresity-Industries/Script/refs/heads/main/updateOS.ps1 | iex"
