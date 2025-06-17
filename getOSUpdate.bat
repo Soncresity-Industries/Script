761a3cb91920eeafb247360dce237ea47ace4a10
SkyKing_PX <skykingpx@emberforge.dev>
2025-06-17 09:03:18 +0000

    Update Scripts (Unfinished)

--- getOSUpdate.bat
+++ getOSUpdate.bat
@@ -15,4 +15,3 @@ if %errorlevel% neq 0 (
 
 :: Commands to execute in PowerShell
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& {irm https://raw.githubusercontent.com/Soncresity-Industries/Script/refs/heads/main/updateOS.ps1 | iex}"
-pause
--- updateOS.ps1
+++ updateOS.ps1
@@ -1,51 +1,157 @@
 # This is the actual update script for SoncrecityOS
 # It may recieve updates in the future, but will always be downloaded by the System through thr getOSUpdate.ps1 script
 
-# Check if the script is running with administrative privileges
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "This script requires administrative privileges. Please run it as an administrator." -ForegroundColor Red
-    exit 1
+# Define Functions
+function StartScript {
+    # Check if the script is running with administrative privileges
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "This script requires administrative privileges. Please run it as an administrator." -ForegroundColor Red
+        exit 1
+    }
+    Write-Host "  ————————————————————————————————————————————————" -ForegroundColor White
+    Write-Host " /  " -ForegroundColor White -NoNewLine
+    Write-Host "Welcome to the SoncrecityOS Update Wizard" -ForegroundColor Cyan -NoNewLine
+    Write-Host "   /" -ForegroundColor White
+    Write-Host "————————————————————————————————————————————————" -ForegroundColor White
+
+    Start-Sleep 2
 }
-Write-Host "  ——————————————————————————————————————————————————————————————————————————" -ForegroundColor White
-Write-Host " /         " -ForegroundColor White -NoNewLine
-Write-Host "Welcome to the SoncrecityOS Update Wizard" -ForegroundColor Cyan -NoNewLine
-Write-Host "         /" -ForegroundColor White
-Write-Host "——————————————————————————————————————————————————————————————————————————" -ForegroundColor White
 
-Write-Host " -=> Pick an Option from below by pressing the corresponding number Key <=-"
-Write-Host "———————————————————————————————————————————————————————————————————————————"
-Write-Host " - (1) 1: Activate Windows or Office 365"
-Write-Host " - (2) 2: List Files in Current Directory"
-Write-Host " - (3) 3: Exit"
-Write-Host " - (4) 4: Exit"
-Write-Host " - (5) 5: Exit"
-Write-Host "——————————————————————————————————————————————————————————————————————————"
+function ListIconThemes {
+    Write-Host "            -=> Available Themes <=-            " -ForegroundColor Yellow
+    Write-Host "/——————————————————————————————————————————————\" -ForegroundColor White
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "1" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Activate Windows or Office 365          " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
 
-# Prompt user for input
-$choice = Read-Host "Please select an option (1-3)"
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "2" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Change Icon Theme                       " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "3" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Install Applications                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "4" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Exit                                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "5" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Exit                                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "\——————————————————————————————————————————————/" -ForegroundColor White
+
+    $choice = Read-Host "Please select an Option from below by pressing the corresponding number Key (1-5)"
+}
+
+function ListOptions {
+
+    Write-Host "            -=> Available Options <=-           " -ForegroundColor Yellow
+    Write-Host "/——————————————————————————————————————————————\" -ForegroundColor White
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "1" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Activate Windows or Office 365          " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "2" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Change Icon Theme                       " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "3" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Install Applications                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "4" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Exit                                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "| " -NoNewLine -ForegroundColor White
+    Write-Host "[" -NoNewLine -ForegroundColor Green
+    Write-Host "5" -NoNewLine -ForegroundColor Cyan
+    Write-Host "]" -NoNewLine -ForegroundColor Green
+    Write-Host ":" -NoNewLine -ForegroundColor White
+    Write-Host " Exit                                    " -NoNewLine -ForegroundColor Magenta
+    Write-Host "|" -ForegroundColor White
+
+    Write-Host "\——————————————————————————————————————————————/" -ForegroundColor White
+
+    $choice = Read-Host "Please select an Option from below by pressing the corresponding number Key (1-5)"
 
 # Handle user input
-switch ($choice) {
-    "1" {
-        Write-Host "You selected Option 1: Displaying Date and Time..."
-        Get-Date
-    }
-    "2" {
-        Write-Host "You selected Option 2: Listing Files in Current Directory..."
-        Get-ChildItem
-    }
-    "3" {
-        Write-Host "You selected Option 3: Exiting..."
+    switch ($choice) {
+        "1" {
+            Write-Host "You selected Option 1: Activate Windows or Office 365"
+            Read-Host "Press Enter to continue..."
+
+            Write-Host "Activating Windows. Please follow the instructions on the screen." -ForegroundColor Green
+            Write-Host "Please close the activation window once the activation is complete to continue" -ForegroundColor Yellow
+            Start-Process powershell -ArgumentList '-NoExit', '-Command', 'irm https://get.activated.win | iex'
+        }
+        "2" {
+            Write-Host "You selected Option 2: Change Icon Theme"
+            $
+        }
+        "3" {
+            Write-Host "You selected Option 3: Exiting..."
         exit
-    }
-    default {
-        Write-Host "Invalid selection. Please run the script again and choose a valid option."
+        }
+        "4" {
+            Write-Host "You selected Option 4: Exiting..."
+            exit
+        }
+        "5" {
+            Write-Host "You selected Option 5: Exiting..."
+            exit
+        }
+        default {
+            Write-Host "Invalid selection. Please run the script again and choose a valid option."
+        }
     }
 }
 
-
-$isWindowsActivated = Read-Host "Is your Windows activated? (y/n - default: n)"
-if ($isWindowsActivated.ToLower -eq "y" -or $isWindowsActivated -eq "") {
-    Write-Host "Activating Windows. Please follow the instructions on the screen." -ForegroundColor Green
-    pwsh -c "irm https://get.activated.win | iex"
+function ExitScript {
+    Write-Host "Exiting the script. Thank you for using SoncrecityOS!" -ForegroundColor Green
+    Start-Sleep 2
+    exit 0
 }
+
+
+# Script Start
+StartScript
+ListOptions
