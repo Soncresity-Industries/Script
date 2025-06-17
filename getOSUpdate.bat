a6da310b1694b2d56741855b69f3a79d6dadcb9d
SkyKing_PX <skykingpx@emberforge.dev>
2025-06-17 07:30:24 +0200

    Update getOSUpdate.bat

--- getOSUpdate.bat
+++ getOSUpdate.bat
@@ -5,34 +5,13 @@ REM | This is a PowerShell script to get the actual update script for Soncresity
 REM | It is included in the System by default and (hopefully) won't ever change    |
 REM |------------------------------------------------------------------------------|
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+:: Check if running as admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~f0' -ArgumentList '-RunAsAdmin' -Verb RunAs"
+    exit /b
 )
 
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------  
-
-pwsh -c "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force"
-pwsh -c "irm https://raw.githubusercontent.com/Soncresity-Industries/Script/refs/heads/main/updateOS.ps1 | iex"
+:: Commands to execute in PowerShell
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {irm https://raw.githubusercontent.com/Soncresity-Industries/Script/refs/heads/main/updateOS.ps1 | iex}"
